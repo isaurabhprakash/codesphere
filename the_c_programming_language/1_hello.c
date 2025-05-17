28c2334fc7ce4805319928898c92351836acfea8
Saurabh Prakash <isaurabhprakash@gmail.com>
2025-05-17 21:14:55 +0530

    Add comments to hello.c program

--- the_c_programming_language/1_hello.c
+++ the_c_programming_language/1_hello.c
@@ -1,6 +1,12 @@
+// tells the compiler to include infromation about standard library
 #include <stdio.h>
 
+// the main function, this is called by default when the program runs. return type is int
 int main()
 {
+    // calls the function printf from the standard library to print the sequene of characters
+    // "hello, world" - hello, world is called a character string or a string constant. \n is a ecape sequence
     printf("hello, world\n");
+
+    // no need to return 0, by default 0 is returned on success
 }
